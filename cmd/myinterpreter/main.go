08fb33625ef55fad1ac3d3fa6fe28e640f8f7e42
Kaio Delphino <github@kaiodelphino.com>
2024-07-12 17:30:07 -0300

    chore: remove print

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -6,9 +6,6 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
-
 	if len(os.Args) < 3 {
 		fmt.Fprintln(os.Stderr, "Usage: ./your_program.sh tokenize <filename>")
 		os.Exit(1)
