d87118254cd31dda4029ca832b0f78fc4f274602
Kaio Delphino <github@kaiodelphino.com>
2024-07-14 23:33:22 -0300

    CLI tests (cli-test-1721010802314)

--- cmd/myinterpreter/main.go
+++ cmd/myinterpreter/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"unicode"
 )
 
 type TokenType = string
@@ -29,6 +31,7 @@ const (
 	LESS_EQUAL    TokenType = "LESS_EQUAL"
 	EOF           TokenType = "EOF"
 	STRING        TokenType = "STRING"
+	NUMBER        TokenType = "NUMBER"
 )
 
 type Token struct {
@@ -154,7 +157,14 @@ func main() {
 			lexeme := `"` + literal + `"`
 			tokens = append(tokens, Token{STRING, lexeme, literal})
 		default:
-			reportError(line, fmt.Errorf("Unexpected character: %s", string(character)))
+			if unicode.IsDigit(character) {
+				tokenizeNumber()
+				// the tokenizeNumber already advances the cursor
+				// that's why we must go to the next iteration manually
+				continue
+			} else {
+				reportError(line, fmt.Errorf("Unexpected character: %s", string(character)))
+			}
 		}
 
 		advanceCursor()
@@ -164,13 +174,14 @@ func main() {
 
 	// print all tokens
 	for _, token := range tokens {
-		var name string
+		var name any
 		if token.Literal == nil {
 			name = "null"
 		} else {
-			name = fmt.Sprintf("%v", token.Literal)
+			// name = fmt.Sprintf("%v", token.Literal)
+      name = token.Literal
 		}
-		fmt.Fprintf(os.Stdout, "%v %s %s\n", token.Type, token.Lexeme, name)
+		fmt.Fprintf(os.Stdout, "%v %s %v\n", token.Type, token.Lexeme, name)
 	}
 
 	// check for errors and print them all
@@ -214,4 +225,28 @@ func currentRuneEquals(target rune) bool {
 
 func nextRuneEquals(target rune) bool {
 	return nextRune() == target
+}
+
+func tokenizeNumber() {
+	startPosition := position
+
+	for unicode.IsDigit(currentRune()) {
+		advanceCursor()
+	}
+
+	if currentRune() == '.' && unicode.IsDigit(nextRune()) {
+		advanceCursor()
+
+		for unicode.IsDigit(currentRune()) {
+			advanceCursor()
+		}
+	}
+
+	lexeme := string(fileContents[startPosition:position])
+	literal, err := strconv.ParseFloat(lexeme, 64)
+	if err != nil {
+		panic(err)
+	}
+	token := Token{NUMBER, lexeme, literal}
+	tokens = append(tokens, token)
 }
