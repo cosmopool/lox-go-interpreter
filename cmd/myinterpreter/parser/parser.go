fb98223b0babf64b548a58687847a7152be3c822
Kaio Delphino <github@kaiodelphino.com>
2024-07-17 15:26:59 -0300

    refac: move entities to separated file

--- cmd/myinterpreter/parser/parser.go
+++ cmd/myinterpreter/parser/parser.go
@@ -1,55 +1,8 @@
 package parser
 
 import (
-	"fmt"
-
 	"github.com/codecrafters-io/interpreter-starter-go/cmd/myinterpreter/scanner"
-	"github.com/codecrafters-io/interpreter-starter-go/cmd/myinterpreter/utils"
 )
-
-type Expression interface {
-	Expression()
-}
-
-type Binary struct {
-	Left     Expression
-	Operator scanner.Token
-	Right    Expression
-}
-
-func (b Binary) String() string {
-	return fmt.Sprintf("(%s (%s %s))", b.Operator.Lexeme, b.Left, b.Right)
-}
-
-func (b Binary) Expression() {}
-
-type Grouping struct {
-	Expr Expression
-}
-
-func (g Grouping) Expression() {}
-
-type Literal struct {
-	Value any
-}
-
-func (l Literal) Expression() {}
-
-func (l Literal) String() string {
-	return utils.VariableToString(l.Value, false)
-}
-
-type Unary struct {
-	Operator scanner.Token
-	Right    Expression
-}
-
-func (u Unary) Expression() {}
-
-type Error struct {
-	Line int
-	Err  error
-}
 
 type Parser struct {
 	Tokens      []scanner.Token
---
+++ cmd/myinterpreter/scanner/entities.go
@@ -0,0 +1,88 @@
+package scanner
+
+import (
+	"fmt"
+
+	"github.com/codecrafters-io/interpreter-starter-go/cmd/myinterpreter/utils"
+)
+
+type tokenType = string
+type keyword = string
+
+const (
+	LEFT_PAREN    tokenType = "LEFT_PAREN"
+	RIGHT_PAREN   tokenType = "RIGHT_PAREN"
+	LEFT_BRACE    tokenType = "LEFT_BRACE"
+	RIGHT_BRACE   tokenType = "RIGHT_BRACE"
+	COMMA         tokenType = "COMMA"
+	DOT           tokenType = "DOT"
+	MINUS         tokenType = "MINUS"
+	PLUS          tokenType = "PLUS"
+	SEMICOLON     tokenType = "SEMICOLON"
+	SLASH         tokenType = "SLASH"
+	STAR          tokenType = "STAR"
+	BANG          tokenType = "BANG"
+	BANG_EQUAL    tokenType = "BANG_EQUAL"
+	EQUAL         tokenType = "EQUAL"
+	EQUAL_EQUAL   tokenType = "EQUAL_EQUAL"
+	GREATER       tokenType = "GREATER"
+	GREATER_EQUAL tokenType = "GREATER_EQUAL"
+	LESS          tokenType = "LESS"
+	LESS_EQUAL    tokenType = "LESS_EQUAL"
+	EOF           tokenType = "EOF"
+	STRING        tokenType = "STRING"
+	NUMBER        tokenType = "NUMBER"
+	IDENTIFIER    tokenType = "IDENTIFIER"
+	AND           keyword   = "AND"
+	CLASS         keyword   = "CLASS"
+	ELSE          keyword   = "ELSE"
+	FALSE         keyword   = "FALSE"
+	FOR           keyword   = "FOR"
+	FUN           keyword   = "FUN"
+	IF            keyword   = "IF"
+	NIL           keyword   = "NIL"
+	OR            keyword   = "OR"
+	PRINT         keyword   = "PRINT"
+	RETURN        keyword   = "RETURN"
+	SUPER         keyword   = "SUPER"
+	THIS          keyword   = "THIS"
+	TRUE          keyword   = "TRUE"
+	VAR           keyword   = "VAR"
+	WHILE         keyword   = "WHILE"
+)
+
+func Keywords() map[string]tokenType {
+	return map[string]tokenType{
+		"and":    AND,
+		"class":  CLASS,
+		"else":   ELSE,
+		"false":  FALSE,
+		"for":    FOR,
+		"fun":    FUN,
+		"if":     IF,
+		"nil":    NIL,
+		"or":     OR,
+		"print":  PRINT,
+		"return": RETURN,
+		"super":  SUPER,
+		"this":   THIS,
+		"true":   TRUE,
+		"var":    VAR,
+		"while":  WHILE,
+	}
+}
+
+type Token struct {
+	Type    tokenType
+	Lexeme  string
+	Literal any
+}
+
+func (t Token) String() string {
+	return fmt.Sprintf("%v %s %v\n", t.Type, t.Lexeme, utils.VariableToString(t.Literal, true))
+}
+
+type Error struct {
+	Line int
+	Err  error
+}
